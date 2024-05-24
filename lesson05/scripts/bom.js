164797b01d66dc8a67b4491eab8f2b3dcf6c5f8e
Brigham Aviles <bandbwebservices@gmail.com>
2024-05-23 19:22:16 -0600

    31

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,3 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('_____'); //fill in the blank to reference the unord
