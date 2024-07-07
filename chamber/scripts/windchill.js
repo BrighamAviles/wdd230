6d9afa09bf161c1acd8fcf3e248f6e6952ed7166
Brigham Aviles <bandbwebservices@gmail.com>
2024-07-06 18:35:57 -0600

    css

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,32 +1,5 @@
 
 
-// let tempElement = document.getElementById('temp');
-// const windSpeedElement = document.getElementById('wind-speed');
-// const windChillElement = document.getElementById('wind-chill');
-
-// // Get the values from the elements and parse them
-// let temp = tempElement.textContent;
-// const windSpeed = parseFloat(windSpeedElement.textContent);
-
-// // Function to calculate wind chill
-// function calculateWindChill(temp, windSpeed) {
-//     // Check if the values meet the specification limits
-//     if (temp <= 50 && windSpeed > 3.0) {
-//         // Calculate the wind chill using the formula
-//         const windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
-//         return windChill.toFixed(2); // Round to 2 decimal places
-//     } else {
-//         return "N/A";
-//     }
-// }
-
-// // Calculate the wind chill and update the HTML
-// const windChill = calculateWindChill(temp, windSpeed);
-// windChillElement.textContent = windChill;
-// console.log(temp)
-
-// Function to calculate wind chill
-// Function to calculate wind chill
 function calculateWindChill(temp, windspeed) {
     if (temp <= 50 && windspeed > 3) {
         let windChill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16));
